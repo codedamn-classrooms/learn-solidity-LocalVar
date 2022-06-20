edf8567c582285c6a310da7117cdad3d298bb464
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-06-20 20:50:04 +0530

    Update LocalVar.sol

--- contracts/LocalVar.sol
+++ contracts/LocalVar.sol
@@ -2,8 +2,5 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract LocalVar {
-    function getLocal() public pure returns (uint256) {
-        uint256 rollNumber = 10;
-        return rollNumber;
-    }
+    
 }
